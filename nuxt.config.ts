eeeae27b2b6c3e9b4ad73d7a3220f1c2f56725b5
Lennart Zellmer <lennart@zellmer.dev>
2022-02-22 02:02:13 +0100

    Transpile prismjs on build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,25 +2,26 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-    css: ['@/assets/css/main.css'],
+  css: ['@/assets/css/main.css'],
 
-    modules: [
-        [
-            '@nuxtjs/prismic',
-            {
-                endpoint: 'https://zellmer-dev.cdn.prismic.io/api/v2'
-            }
-        ]
+  modules: [
+    [
+      '@nuxtjs/prismic',
+      {
+        endpoint: 'https://zellmer-dev.cdn.prismic.io/api/v2',
+      },
     ],
+  ],
 
-    build: {
-        postcss: {
-            postcssOptions: {
-                plugins: {
-                    tailwindcss: {},
-                    autoprefixer: {},
-                },
-            },
+  build: {
+    transpile: ['prismjs'],
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
         },
+      },
     },
+  },
 })
