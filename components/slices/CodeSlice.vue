8ad9148bd7673d95b722948fff06d3dd5a25fbdf
Lennart Zellmer <lennart@zellmer.dev>
2022-02-24 10:19:07 +0100

    Replace prismjs with highlightjs

--- components/slices/CodeSlice.vue
+++ components/slices/CodeSlice.vue
@@ -1,38 +1,28 @@
 <template>
   <pre
-    class="rounded-lg"
-  ><code :class="'language-' + slice.primary.language" >{{ $prismic.asText(slice.primary.code) }}</code>
+    class="rounded-lg overflow-hidden"
+  ><highlightjs :language="slice.primary.language" :code="$prismic.asText(slice.primary.code)" />
   </pre>
 </template>
 
 <script lang="ts">
-import { highlightAll } from 'prismjs'
-import { defineComponent, onMounted } from '#imports'
-import '~/assets/css/prism-theme.css'
-
-import 'prismjs/components/prism-markup-templating'
-import 'prismjs/components/prism-css'
-import 'prismjs/components/prism-php'
-import 'prismjs/components/prism-json'
-import 'prismjs/components/prism-javascript'
-import 'prismjs/components/prism-jsx'
-import 'prismjs/components/prism-bash'
-import 'prismjs/components/prism-yaml'
-import 'prismjs/components/prism-toml'
+import 'highlight.js/styles/github.css'
+import 'highlight.js/lib/common'
+import hljsVuePlugin from '@highlightjs/vue-plugin'
+import { defineComponent } from '#imports'
 
 export default defineComponent({
   name: 'CodeSlice',
+  components: {
+    highlightjs: hljsVuePlugin.component,
+  },
   props: {
     slice: {
       required: true,
       type: Object,
     },
   },
-  setup() {
-    onMounted(() => {
-      window.Prism = window.Prism || {}
-      highlightAll() // highlight your code on mount
-    })
-  },
+  setup() {},
+  mounted() {},
 })
 </script>
