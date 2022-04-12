6be0f56fc159fc2d2e12a30c9b22db69ca25a0eb
Lennart Zellmer <lennart@zellmer.dev>
2022-04-13 00:13:49 +0200

    Fix color setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,33 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
-const colors = require('tailwindcss/colors')
+const radixColors = require('@radix-ui/colors')
+
+function colorKey(name, step) {
+  return `${name.replace('Dark', '')}${step}`
+}
+
+function fromRadixColor(key, color) {
+  return [
+    key,
+    {
+      1: color[colorKey(key, 1)],
+      2: color[colorKey(key, 2)],
+      3: color[colorKey(key, 3)],
+      4: color[colorKey(key, 4)],
+      5: color[colorKey(key, 5)],
+      6: color[colorKey(key, 6)],
+      7: color[colorKey(key, 7)],
+      8: color[colorKey(key, 8)],
+      9: color[colorKey(key, 9)],
+      10: color[colorKey(key, 10)],
+      11: color[colorKey(key, 11)],
+      12: color[colorKey(key, 12)],
+    },
+  ]
+}
+
+const mappedRadixColors = Object.fromEntries(
+  Object.entries(radixColors).map(([key, value]) => fromRadixColor(key, value))
+)
 
 module.exports = {
   content: [
@@ -8,6 +36,7 @@ module.exports = {
     './pages/**/*.vue',
     './plugins/**/*.{js,ts}',
   ],
+  darkMode: 'class',
   safelist: ['line-through'],
   theme: {
     fontFamily: {
@@ -29,36 +58,24 @@ module.exports = {
         },
       },
       colors: {
-        gray: colors.stone,
-        green: colors.green,
-        yellow: colors.yellow,
-        primary: {
-          100: '#F8FAF0',
-          200: '#EBF1D5',
-          300: '#DDE8B9',
-          400: '#A4AD86',
-          500: '#6A7152',
-          600: '#565C43',
-          700: '#424733',
-          800: '#2C3022',
-          900: '#161811',
-        },
-        slate: {
-          1: '#fbfcfd',
-          2: '#f8f9fa',
-          3: '#f1f3f5',
-          4: '#eceef0',
-          5: '#e6e8eb',
-          6: '#dfe3e6',
-          7: '#d7dbdf',
-          8: '#c1c8cd',
-          9: '#889096',
-          10: '#7e868c',
-          11: '#687076',
-          12: '#11181c',
-        },
-        action: {
-          red: '#F34616',
+        yellow: mappedRadixColors.amber,
+        'yellow-dark': mappedRadixColors.amberDark,
+        red: mappedRadixColors.red,
+        'red-dark': mappedRadixColors.redDark,
+        blue: mappedRadixColors.blue,
+        'blue-dark': mappedRadixColors.blueDark,
+        slate: mappedRadixColors.slate,
+        'slate-dark': mappedRadixColors.slateDark,
+        gray: {
+          100: mappedRadixColors.slate[1],
+          200: mappedRadixColors.slate[3],
+          300: mappedRadixColors.slate[5],
+          400: mappedRadixColors.slate[7],
+          500: mappedRadixColors.slate[8],
+          600: mappedRadixColors.slate[9],
+          700: mappedRadixColors.slate[10],
+          800: mappedRadixColors.slate[11],
+          900: mappedRadixColors.slate[12],
         },
       },
     },
