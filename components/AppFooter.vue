6c536f02be7c10819f4ac9e7f013e782a9b088f7
Lennart Zellmer <lennart@zellmer.dev>
2022-09-12 23:51:52 +0200

    Tweak footer dark mode

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -5,40 +5,39 @@
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 1440 167"
-        class="w-full h-16 md:h-24 lg:h-32"
+        class="w-full h-16 md:h-24 lg:h-32 text-slate-3 dark:text-slate-dark-1"
         preserveAspectRatio="none"
       >
         <path
           d="M1440 167.709C883.5 151 262.088 59.415 0 1v166.709h1440Z"
-          fill="#101212"
+          fill="currentColor"
         />
         <path
           d="M1440 167.709C883.5 151 262.088 59.415 0 1"
-          class="stroke-slate-dark-3"
+          class="stroke-slate-6 dark:stroke-slate-dark-3"
           stroke-linecap="round"
         />
       </svg>
     </div>
-    <div class="bg-[#101212] pt-12">
+    <div class="bg-slate-3 dark:bg-slate-dark-1 pt-12">
       <div
         v-if="bio"
-        class="grid grid-cols-12 mx-auto max-w-5xl sm:pb-24 md:gap-8 md:px-4"
+        class="grid grid-cols-12 mx-auto max-w-5xl sm:pb-24 md:gap-16 md:px-4 mt-8"
       >
-        <div class="flex col-span-10 col-start-2 md:col-span-5 md:col-start-1">
-          <PrismicImage
-            :imgix-params="{ fit: 'crop', h: 800, w: 800 }"
-            :pixel-densities="[1, 2]"
-            :field="bio.data.profile_image"
-            class="object-contain opacity-100 mix-blend-lighten brightness-105"
-          />
-        </div>
-
         <div
-          class="flex items-center mt-8 md:col-span-7 md:col-start-auto col-span-10 col-start-2"
+          class="flex items-center md:col-span-7 md:col-start-auto col-span-10 col-start-2"
         >
           <div class="prismic-text-dark">
             <PrismicRichText :field="bio.data.bio_text" />
           </div>
+        </div>
+        <div class="flex col-span-10 md:col-span-5 rotate-3">
+          <PrismicImage
+            :imgix-params="{ fit: 'crop', h: 800, w: 800 }"
+            :pixel-densities="[1, 2]"
+            :field="bio.data.profile_image"
+            class="object-contain opacity-100 bg-black rounded-xl self-start"
+          />
         </div>
       </div>
       <footer class="flex justify-center py-12 mx-auto max-w-7xl">
@@ -73,13 +72,13 @@ const { data: bio } = useAsyncData('bio', () =>
 <style lang="scss">
 .prismic-text-dark {
   h3 {
-    @apply text-slate-6 font-bold text-3xl mb-3;
+    @apply text-slate-12 font-bold text-5xl mb-3 dark:text-slate-dark-12;
   }
   p {
-    @apply text-slate-10 leading-relaxed pb-6;
+    @apply text-slate-10 leading-relaxed pt-6 dark:text-slate-dark-10;
   }
   a {
-    @apply text-slate-8 underline hover:text-slate-5 leading-relaxed font-mono;
+    @apply text-slate-12 dark:text-slate-dark-11 dark:hover:text-slate-dark-10 hover:text-slate-11 leading-relaxed font-mono;
   }
 }
 </style>
