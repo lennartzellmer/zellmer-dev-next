f575e63fd3d0aef249ae876426a467dbdbe60032
Lennart Zellmer <lennart@zellmer.dev>
2022-09-21 23:02:40 +0200

    Lazy load code slice

--- components/slices/SlicesBlock.vue
+++ components/slices/SlicesBlock.vue
@@ -2,17 +2,17 @@
   <article>
     <template v-for="(slice, index) in props.slices">
       <template v-if="slice.slice_type === 'text'">
-        <TextSlice
+        <SlicesTextSlice
           :key="'slice-' + index"
           class="prose prose-lg prose-slate max-w-none dark:prose-invert"
           :slice="slice"
         />
       </template>
       <template v-if="slice.slice_type === 'image'">
-        <ImageSlice :key="'slice-' + index" :slice="slice" />
+        <SlicesImageSlice :key="'slice-' + index" :slice="slice" />
       </template>
       <template v-if="slice.slice_type === 'code'">
-        <CodeSlice :key="'slice-' + index" :slice="slice" />
+        <LazySlicesCodeSlice :key="'slice-' + index" :slice="slice" />
       </template>
     </template>
   </article>
@@ -20,9 +20,6 @@
 
 <script lang="ts" setup>
 import { Slice } from '@prismicio/types'
-import TextSlice from '@/components/slices/TextSlice.vue'
-import ImageSlice from '@/components/slices/ImageSlice.vue'
-import CodeSlice from '@/components/slices/CodeSlice.vue'
 
 const props = defineProps<{
   slices: Slice<any>[]
