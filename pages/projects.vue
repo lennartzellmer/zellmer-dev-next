647270f3c3c83908719f8288d369172dac2850de
Lennart Zellmer <lennart@zellmer.dev>
2022-10-26 00:30:37 +0200

    Remove analytics

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="mx-auto mt-8 mt-16 w-full max-w-5xl px-4 sm:mt-32 lg:px-0">
+  <section class="mx-auto mt-16 w-full max-w-5xl px-4 sm:mt-32 lg:px-0">
     <header class="max-w-2xl">
       <h1
         class="text-4xl font-bold tracking-tight text-slate-12 dark:text-slate-dark-12 sm:text-5xl"
@@ -7,8 +7,8 @@
         Here are some things I am quite proud of
       </h1>
       <p class="mt-6 text-base text-slate-11 dark:text-slate-dark-11">
-        I’ve worked on tons of little projects over the years but these are the
-        ones that I’m most proud of.
+        I've worked on tons of little projects over the years but these are the
+        ones that I'm most proud of.
       </p>
     </header>
     <div class="mt-16 mb-28 sm:mt-20">
