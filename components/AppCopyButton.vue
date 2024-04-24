62e866b534b513185f3814e7ce6c708ec62435c7
Lennart Zellmer <lennart@zellmer.dev>
2024-04-24 14:21:04 +0200

    Add copy button

---
+++ components/AppCopyButton.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+import { useClipboard } from '@vueuse/core'
+
+interface Props {
+  textToCopy: string
+}
+
+defineProps<Props>()
+
+const { copy, copied, isSupported } = useClipboard()
+</script>
+
+<template>
+  <div v-if="isSupported">
+    <button
+      class=" border-slate-dark-7 bg-slate-dark-5 text-slate-dark-11 hover:text-slate-dark-12 rounded border size-10 hover:bg-slate-dark-6 transition-colors"
+      @click="copy(textToCopy)"
+    >
+      <span v-if="!copied">
+        <Icon
+          name="ph:copy"
+        />
+        <span class="sr-only">Copy</span>
+      </span>
+      <span v-else>
+        <Icon
+          name="ph:check-square-offset"
+        />
+        <span class="sr-only">Copied</span>
+      </span>
+    </button>
+  </div>
+</template>
--- slices/Code/index.vue
+++ slices/Code/index.vue
@@ -2,6 +2,7 @@
 <script setup lang="ts">
 import { type Content } from '@prismicio/client'
 import { asText } from '@prismicio/client'
+import AppCopyButton from '~/components/AppCopyButton.vue'
 
 // The array passed to `getSliceComponentProps` is purely optional.
 // Consider it as a visual hint for you when templating your slice.
@@ -16,14 +17,20 @@ defineProps(
 </script>
 
 <template>
-  <Shiki
-    :data-slice-type="slice.slice_type"
-    :data-slice-variation="slice.variation"
-    lang="js"
-    :code="asText(slice.primary.code)"
-    as="code"
-    class="my-6 block"
-  />
+  <div class="relative">
+    <AppCopyButton
+      class="absolute top-2 right-2"
+      :text-to-copy="asText(slice.primary.code)"
+    />
+    <Shiki
+      :data-slice-type="slice.slice_type"
+      :data-slice-variation="slice.variation"
+      lang="js"
+      :code="asText(slice.primary.code)"
+      as="code"
+      class="my-6 block"
+    />
+  </div>
 </template>
 
 <style lang="scss">
