52e3a8831b10a7dde497f83b1f11eeccc69a1dcc
lennartzellmer <2241624+lennartzellmer@users.noreply.github.com>
2026-01-05 14:34:21 +0100

    Improve footer spaceing

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -1,21 +1,21 @@
 <template>
-  <footer class="mx-auto my-12 flex max-w-5xl flex-row items-center justify-between py-4 sm:mt-8">
-    <nav class="mr-auto flex items-center rounded-full text-sm font-medium sm:ml-3 sm:text-base">
+  <footer class="mx-auto my-12 flex max-w-5xl flex-row items-center justify-between py-4 sm:mt-8 px-4 sm:px-0">
+    <nav class="flex items-center gap-4 rounded-full text-sm font-medium sm:text-base">
       <a
         href="https://github.com/lennartzellmer"
         target="_blank"
         rel="noopener noreferrer"
-        class="flex items-center px-3 py-2 text-slate-11 transition hover:text-green-500 dark:text-slate-dark-11"
+        class="flex items-center py-2 text-slate-11 transition hover:text-green-500 dark:text-slate-dark-11"
       >
         <span class="text-base">Source</span>
       </a>
       <NuxtLink
         to="/imprint"
-        class="flex items-center px-3 py-2 text-slate-11 transition hover:text-green-500 dark:text-slate-dark-11"
+        class="flex items-center py-2 text-slate-11 transition hover:text-green-500 dark:text-slate-dark-11"
       >
         <span class="text-base">Imprint</span>
       </nuxtlink>
     </nav>
-    <span class=" text-slate-11 font-mono px-3 py-2">lennart.zellmer.dev</span>
+    <span class=" text-slate-11 font-mono py-2">lennart.zellmer.dev</span>
   </footer>
 </template>
